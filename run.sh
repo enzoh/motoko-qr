4791bc4d3b43485029b5e8d26fe3ebe00df23ef0
Enzo Haussecker <enzo@sovereign.io>
2020-07-11 09:50:03 -0700

    Trivial remove redundant echos

--- run.sh
+++ run.sh
@@ -20,14 +20,10 @@ echo
 echo == Demo.
 echo
 
-echo
 echo 01234567
 echo
 dfx canister call demo encode '(variant{Version = 1}, variant{M}, variant{Numeric}, "01234567")'  | tr -d '(")' | sed 's/#/█/g'
 echo
-
-echo
 echo HELLO WORLD
 echo
 dfx canister call demo encode '(variant{Version = 1}, variant{Q}, variant{Alphanumeric}, "HELLO WORLD")'  | tr -d '(")' | sed 's/#/█/g'
-echo
